35162964dd35f5b65983b37d1179da78dd765b3a
XuLei <xulei@XuLeis-MBP.lan>
2016-07-28 18:57:12 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import React from 'react';
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            input:'/*add your jsx here*/',
+            output:'',
+            error:''
+        }
+        this.update = this.update.bind(this);
+    }
+    update(e){
+        let code = e.target.value;
+        try {
+            this.setState({
+                output:babel.transform(code,{
+                    stage:0,
+                    loose:'all'
+                }).code,
+                err:''
+            });
+        }
+        catch (error){
+            this.setState({error:error.message});
+        }
+    }
+    render(){
+        return (
+            <div>
+                <header>{this.state.error}</header>
+                <div className="container">
+                    <textarea
+                        onChange={this.update}
+                        defaultValue={this.state.input}
+                    ></textarea>
+                    <pre>
+                        {this.state.output}
+                    </pre>
+                </div>
+            </div>
+        )
+    }
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: './',
+        filename:'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    module: {
+        loaders:[
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets:['es2015','react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+            }
+        ]
+    }
+}
